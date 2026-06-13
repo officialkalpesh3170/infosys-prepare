1e01aa82ccc2062abb96a024afad0fb8154f1506
officialkalpesh3170 <officialkalpesh3170@gmail.com>
2026-06-14 02:24:10 +0530

    commit

--- fastapi/main.py
+++ fastapi/main.py
@@ -4,3 +4,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}       
+
+@app.get("/about")
+def about():
+    return {"About": "This is a simple FastAPI application."
+            "It demonstrates how to create a basic API with FastAPI."}
+
+@app.get("/contactus")
+def contact_us():
+    return {"Contact": "You can reach us at contact@example.com."}
