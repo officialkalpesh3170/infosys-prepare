86ccda8153f5de2ca4d9ddcc395d061ebfd80c12
officialkalpesh3170 <officialkalpesh3170@gmail.com>
2026-06-14 23:47:08 +0530

    Refactor API endpoints and enhance functionality for user creation and contact queries

--- fastapi/main.py
+++ fastapi/main.py
@@ -4,12 +4,39 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}       
+# dynamic path parameter 
 
-@app.get("/about")
-def about():
-    return {"About": "This is a simple FastAPI application."
-            "It demonstrates how to create a basic API with FastAPI."}
+
+@app.get("/about/{name}")
+def about(name:str):
+    return {"About":  "This is a simple FastAPI application.",
+            "name": name}
+
+# QUERY PARAMETER (?name=value)
+
 
 @app.get("/contactus")
-def contact_us():
-    return {"Contact": "You can reach us at contact@example.com."}
+def contact_us(name:str=None ,price:int=0):
+    return {"Contact": "Found your in query parameter",
+            "name": name,
+            "price": price}
+
+# POST REQUEST
+
+@app.post("/create")
+def create_item(name:str, price:float):
+    return {"message": "Item created successfully",
+            "name": name,
+            "price": price}
+
+# Pydantic model for request body
+from pydantic import BaseModel
+class User(BaseModel):
+    name: str
+    email: str  
+    age: int    
+
+@app.post("/create_user")
+def create_user(user:User):
+    return {"message": "User created successfully",
+            "user": user}
